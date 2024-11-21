6736f3e78396cd22e3cd0af038cc172007eec872
Benedict Hazel <bwhazel@gmail.com>
2024-11-21 23:11:05 +0000

    Simplify action logic and update it to be platform-agnostic.

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,13 @@
 FROM python:3.13.0-alpine3.20
 LABEL maintainer="Benedict Hazel <bwhazel@gmail.com>"
 
+ENV QUBIT_STATE_0="1.0"
+ENV QUBIT_STATE_1="0.0"
+ENV GATE="I"
+ENV OUTPUT="github"
+
 RUN pip install numpy
 
 COPY action/*.py /action/
 
-ENTRYPOINT [ "python", "/action/main.py" ]
+ENTRYPOINT python /action/main.py --qubit-state-0 ${QUBIT_STATE_0} --qubit-state-1 ${QUBIT_STATE_1} --gate ${GATE} --output ${OUTPUT}
--- action/main.py
+++ action/main.py
@@ -2,6 +2,8 @@
 Applies a quantum gate to a qubit.
 """
 
+from argparse import ArgumentParser, Namespace
+import io
 import os
 import numpy as np
 from gates import get_gate_matrix
@@ -12,31 +14,54 @@ from qubit import (
     is_valid_qubit_state
 )
 
-qubit_state_0: str = os.environ['QUBIT_STATE_0']
-qubit_state_1: str = os.environ['QUBIT_STATE_1']
-gate: str = os.environ['GATE']
+parser: ArgumentParser = ArgumentParser(description='Applies a quantum gate to a qubit.')
+parser.add_argument('-0', '--qubit-state-0',
+                    type=float,
+                    dest='qubit_state_0',
+                    default=1.0,
+                    help='The qubit 0 component.')
+parser.add_argument('-1', '--qubit-state-1',
+                    type=float,
+                    dest='qubit_state_1',
+                    default=0.0,
+                    help='The qubit 1 component.')
+parser.add_argument('-g', '--gate',
+                    type=str,
+                    dest='gate',
+                    choices=['H', 'I', 'X', 'Z'],
+                    default='I',
+                    help='The quantum gate to apply.')
+parser.add_argument('-o', '--output',
+                    type=str,
+                    dest='output',
+                    choices=['github', 'stdout'],
+                    default='stdout',
+                    help='The output target.')
+arguments: Namespace = parser.parse_args()
 
-def main():
-    """
-    Program entry point.
-    """
-    qubit_0: float = float(qubit_state_0)
-    qubit_1: float = float(qubit_state_1)
-    if not is_valid_qubit_state(qubit_0, qubit_1):
-        raise ValueError('Invalid qubit state.')
+qubit_state_0: float = arguments.qubit_state_0
+qubit_state_1: float = arguments.qubit_state_1
+gate: str = arguments.gate
+output: str = arguments.output
 
-    qubit: np.ndarray = create_qubit(qubit_0, qubit_1)
+if not is_valid_qubit_state(qubit_state_0, qubit_state_1):
+    raise ValueError('Invalid qubit state.')
 
-    gate_matrix: np.ndarray = get_gate_matrix(gate)
-    if gate_matrix is None:
-        raise ValueError('Invalid quantum gate.')
+qubit: np.ndarray = create_qubit(qubit_state_0, qubit_state_1)
 
-    result: np.ndarray = apply_quantum_gate(gate_matrix, qubit, round_digits=4)
-    result_sanitised: list[float] = convert_qubit_to_float(result)
+gate_matrix: np.ndarray = get_gate_matrix(gate)
+if gate_matrix is None:
+    raise ValueError('Invalid quantum gate.')
 
+result: np.ndarray = apply_quantum_gate(gate_matrix, qubit, round_digits=4)
+result_sanitised: list[float] = convert_qubit_to_float(result)
+
+result_output = io.StringIO()
+result_output.write(f'final-qubit-state-0={result_sanitised[0]}\n')
+result_output.write(f'final-qubit-state-1={result_sanitised[1]}')
+
+if output == 'github':
     with open(os.environ['GITHUB_OUTPUT'], 'a', encoding='utf-8') as github_output:
-        github_output.write(f'final-qubit-state-0={result_sanitised[0]}\n')
-        github_output.write(f'final-qubit-state-1={result_sanitised[1]}\n')
-
-if __name__ == '__main__':
-    main()
+        github_output.write(result_output.getvalue())
+else:
+    print(result_output.getvalue())
